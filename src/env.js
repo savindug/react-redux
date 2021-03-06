2ee74998348175870cf11dc4ce7e0dac2d69c96c
Savindu Gunawardena <savibashnon11@gmail.com>
2021-03-06 15:37:25 +0530

    firebase initialize

--- src/env.js
+++ src/env.js
@@ -2,3 +2,6 @@ export const getApiURI = () => {
   const url = 'https://jsonplaceholder.typicode.com';
   return url;
 };
+
+const firebase_ci =
+  '1//0gbeGY4-yQA-zCgYIARAAGBASNwF-L9IrWfVBp9XXyu466JImA8yVQiqYDzqtzlp5p5A6ruxDpBs75pw8bJX39v-T0W7doC5VO9w';
